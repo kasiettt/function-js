66992eeba7979adb3a9932e7eba005993dbdd4bf
Kasiet Tokbaeva <kasiet.tokbaeva.kk@gmail.com>
2023-12-14 21:18:45 +0600

    31github

---
+++ index.js
@@ -0,0 +1,63 @@
+/*function sayHello() {
+    document.write("Hello, Welcome to my website");
+}
+
+sayHello();*/
+
+/*function calculateSum(num1,num2) {
+ let sum = num1 + num2;
+ return sum;
+}
+
+function showSum(sum) {
+    document.write(`сумма двух чисел равна: ${sum}`);
+}
+let sum = calculateSum(5,50);
+showSum(sum);*/
+
+
+/*let sum = function() {
+    let sum = num1 + num2;
+
+    return sum;
+};
+
+showSum(sum(20,40));*/
+
+
+/*let potatoes = 50;
+ function peelPotates(potatoes) {
+    if (potatoes >= 0) {
+        document.write(`Осталось ${potatoes} <br>`);
+        potatoes--;
+        peelPotates(potatoes);
+    }
+ }
+
+ peelPotates(potatoes);*/
+
+ /*let userName = 'Иван Петров';
+ let userAge = '17';
+
+ function showMessage() {
+    let message = `Привет, ${userName} c возрастом ${userAge} лет`;
+    document.write(message);
+ }
+showMessage();*/
+
+
+function sum(num1, num2) {
+    let sum = num1 + num2;
+  
+    return sum;
+  }
+  
+  document.write(sum(10, 20));
+
+
+
+
+
+
+
+
